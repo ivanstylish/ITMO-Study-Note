03f2d1906ca3e249301364080dd41d4423d4e36b
Ivan <147992767+ivanstylish@users.noreply.github.com>
2025-01-21 01:04:43 +0300

    update

--- Database/smtest.sql
+++ Database/smtest.sql
@@ -1,23 +1,33 @@
-create database mybase;  
-\c mybase  
-\l  
-\c postgres  
-drop mybase;  
-create database mybase;  
-\c mybase  
-mybase=# create table STUDENTS(  
-mybase(# STUD_ID integer,  
-mybase(# STUD_NAME text,  
-mybase(# BIRTH_DATE date  
-mybase(# );  
-CREATE TABLE  
-alter table STUDENTS add column STUD_GROUP text;  
-ALTER TABLE  
-select * from students;  
-insert into students values (407959, 'JIAJUN ZHONG', '02/01/2005', 'P3110');  
-INSERT 0 1  
-select * from students;  
+-- 检查数据库是否已存在，如果存在则删除
+DROP DATABASE IF EXISTS mybase;
+
+-- 创建数据库
+CREATE DATABASE mybase;
+
+-- 切换到新创建的数据库
+\c mybase
+
+-- 创建学生表
+CREATE TABLE IF NOT EXISTS STUDENTS (
+    STUD_ID integer,
+    STUD_NAME text,
+    BIRTH_DATE date
+);
+
+-- 添加学生组字段到学生表
+ALTER TABLE IF EXISTS STUDENTS ADD COLUMN IF NOT EXISTS STUD_GROUP text;
+
+-- 插入学生数据
+INSERT INTO STUDENTS (STUD_ID, STUD_NAME, BIRTH_DATE, STUD_GROUP)
+VALUES (407959, 'JIAJUN ZHONG', '02/01/2005', 'P3110')
+ON CONFLICT (STUD_ID) DO NOTHING;
+
+-- 查询所有学生数据
+SELECT * FROM STUDENTS;  
+
+-- 最终展现效果
  stud_id |  stud_name   | birth_date | stud_group  
 ---------+--------------+------------+------------  
   407959 | JIAJUN ZHONG | 2005-01-02 | P3110  
 (1 行记录)  
+
