030e7619bbfe2b44b06fcab92f0746c1d1673a83
Ivan <147992767+ivanstylish@users.noreply.github.com>
2026-01-20 22:06:45 +0300

    UPDATE

--- ProgrammingLanguage/work4/class4/obj/Debug/net8.0/class4.AssemblyInfo.cs
+++ ProgrammingLanguage/work4/class4/obj/Debug/net8.0/class4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("class4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a79e8682db7e01ff26b8aa28c8877352eae734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed865d52c4601c0c31e55a5bf84d3572f37496f")]
 [assembly: System.Reflection.AssemblyProductAttribute("class4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("class4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
